2d56c816bd5e7e55e65906ff75c5cb04ef66330f
Chris Jeong <cjeong@Chriss-MacBook-Pro.local>
2019-10-18 00:31:21 -0500

    Updates

---
+++ precache-manifest.d73b8d96749cb661be4f1845b7cf1404.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121c87413050a7bbb91aac7fca7014f4",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "dd64eb2710ac361d9881",
+    "url": "/movie_app_2019/static/css/main.2cc04857.chunk.css"
+  },
+  {
+    "revision": "9c63e566ee8166e9431f",
+    "url": "/movie_app_2019/static/js/2.6d27e71f.chunk.js"
+  },
+  {
+    "revision": "dd64eb2710ac361d9881",
+    "url": "/movie_app_2019/static/js/main.27b86a36.chunk.js"
+  },
+  {
+    "revision": "420037a5e08d7d6458d7",
+    "url": "/movie_app_2019/static/js/runtime-main.6a961451.js"
+  }
+]);
